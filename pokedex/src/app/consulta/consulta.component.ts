af69ea55f256bbb3d9b9ef63e2d53d71c89475f2
João Gabriel Z.B <71906858+JoaoGabrielBertolucci@users.noreply.github.com>
2023-04-29 09:26:01 -0300

    edição de componentes

--- pokedex/src/app/consulta/consulta.component.ts
+++ pokedex/src/app/consulta/consulta.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { poke } from '../poke';
+import { PokeService } from '../pokee.service';
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
   styleUrls: ['./consulta.component.css']
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit{
+  ngOnInit(): void {
+    
+  }
+
+  LoadDog(){
+    this.PokeService.getRandomDog().subscribe(
+      {
+        next : poke => this.dog = dog
+      }
+    );
+  }
 
 
-  
 }
